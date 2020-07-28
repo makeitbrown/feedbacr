3994f4f98c3134b386a65a1d12f94d7424922a3e
Michael Brown <brown.michael.n@gmail.com>
2020-07-28 09:58:33 -0600

    Initial Creation

---
+++ feedbacr_MVP/feedbacr_MVPTests/feedbacr_MVPTests.swift
@@ -0,0 +1,34 @@
+//
+//  feedbacr_MVPTests.swift
+//  feedbacr_MVPTests
+//
+//  Created by Michael Brown on 7/27/20.
+//  Copyright © 2020 Michael Brown. All rights reserved.
+//
+
+import XCTest
+@testable import feedbacr_MVP
+
+class feedbacr_MVPTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
