d42520964678d0cd8a6f04e6b2ae7553d70dd5b6
Michael Brown <brown.michael.n@gmail.com>
2020-09-22 23:02:31 -0600

    started moveQuestion func

---
+++ feedbacr_MVP/feedbacr_MVP/InterviewQuestionJournalData/JournalData.swift
@@ -0,0 +1,16 @@
+//
+//  JournalData.swift
+//  feedbacr_MVP
+//
+//  Created by Michael Brown on 9/22/20.
+//  Copyright © 2020 Michael Brown. All rights reserved.
+//
+
+import Foundation
+
+func moveQuestion() {
+//    Take a question that is in the UnansweredInterviewQuestions Array and when a response has been imputted, delete it from Unanswered. Move it to answeredQuestions Array.
+    if !UnansweredInterviewQuestions.isEmpty {
+        UnansweredInterviewQuestions[IndexPath]
+    }
+}
